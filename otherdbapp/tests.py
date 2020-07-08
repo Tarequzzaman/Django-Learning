8397db0d7398e9738f6f295801095ab2f2b604ce
Tarequzzaman <tareqcse12@gmail.com>
2020-07-08 16:30:48 +0600

    test case added for other app

--- otherdbapp/tests.py
+++ otherdbapp/tests.py
@@ -1,3 +1,34 @@
-from django.test import TestCase
+import requests
+import unittest
+
+
+from django.test import Client, RequestFactory
+from unittest.mock import patch
+from .views import create_user, get_user
+from django.urls import reverse
 
 # Create your tests here.
+
+
+
+class TestView(unittest.TestCase):
+ 
+    def test_sum(self):
+            self.assertEqual(sum([1, 2, 3]), 6)
+
+    def test_create_users(self):
+        client = Client()
+        response= client.get(reverse(create_user), {'first_name': 'john', 'last_name': 'smith', 'phone': "01838198104", 'email': "tareqcse12@gmail.com"})
+        self.assertEqual(response.status_code, 200)
+
+
+    def test_get_user(self):
+        client = Client()
+        response= client.get(reverse(get_user))
+        self.assertEqual(response.status_code, 450)
+
+
+
+
+if __name__ == '__main__':
+    unittest.main()
