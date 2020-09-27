c9b1979cd1c382d157a6141c2bc131c35762d9ca
Tarequzzaman <tareqcse12@gmail.com>
2020-09-27 18:02:04 +0600

    space added

--- mongoDBwork/urls.py
+++ mongoDBwork/urls.py
@@ -2,6 +2,8 @@ from django.conf.urls import url, include
 from django.contrib import admin
 from .views import create_post, get_post
 urlpatterns = [
+
+    
     url(r"create_post", create_post),
     url(r'get_post', get_post)
 ]
